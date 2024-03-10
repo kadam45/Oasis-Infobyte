790f65a6a02d9cb8f1cdc62ee61fb424107de890
Sanket Kadam <153412193+kadam45@users.noreply.github.com>
2024-03-10 19:35:35 +0530

    Add files via upload

---
+++ Port Folio/index.js
@@ -0,0 +1,30 @@
+// Projects 
+document.getElementById("calculator_git").addEventListener("click",()=>{
+    window.open("https://github.com/kadam45/CodSoft/tree/main/Web%20Development(Level%201)/Calculator")
+});
+document.getElementById("calculator_deployed").addEventListener("click",()=>{
+    window.open("https://melodic-pony-a8e03e.netlify.app/")
+});
+
+document.getElementById("sugarcosmetics_git").addEventListener("click",()=>{
+    window.open("https://github.com/kadam45/CodSoft/tree/main/Web%20Development(Level%201)/Landing%20page")
+})
+document.getElementById("sugarcosmetics_deployed").addEventListener("click",()=>{
+    window.open("https://joyful-cajeta-c2e9a7.netlify.app/")
+})
+
+
+document.getElementById("Temperature Converter_git").addEventListener("click",()=>{
+    window.open("https://github.com/kadam45/Oasis-Infobyte/tree/main/Temperature%20Converter")
+})
+document.getElementById("Temperature Converter_deployed").addEventListener("click",()=>{
+    window.open("")
+})
+
+// Contact
+document.getElementById("githubprofile").addEventListener("click",()=>{
+    window.open("https://github.com/kadam45")
+})
+document.getElementById("linkedinprofile").addEventListener("click",()=>{
+    window.open("https://www.linkedin.com/in/sanket-kadam-0ba2562b8/")
+})
